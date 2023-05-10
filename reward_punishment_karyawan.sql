0cfa29ade39817440d7191d1fa28899e46605700
Andri Firman Saputra <andrifirmansaputra1@gmail.com>
2023-05-10 23:01:26 +0700

    database

---
+++ reward_punishment_karyawan.sql
@@ -0,0 +1,170 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 10 Bulan Mei 2023 pada 18.00
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `reward_punishment_karyawan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `id_karyawan` int(11) NOT NULL,
+  `nama_karyawan` varchar(50) NOT NULL,
+  `jenis_kelamin` enum('L','P') NOT NULL,
+  `alamat_karyawan` text NOT NULL,
+  `whatsapp_karyawan` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `karyawan`
+--
+
+INSERT INTO `karyawan` (`id_karyawan`, `nama_karyawan`, `jenis_kelamin`, `alamat_karyawan`, `whatsapp_karyawan`) VALUES
+(3, 'Andri Firman Saputra', 'L', 'Jl. AMD Babakan Pocis No. 88', '6287808675313');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `punishment`
+--
+
+CREATE TABLE `punishment` (
+  `id_punishment` int(11) NOT NULL,
+  `punishment` enum('TEGURAN LISAN','TEGURAN TERTULIS','SKORSING','PEMBERHENTIAN') NOT NULL,
+  `keterangan` text NOT NULL,
+  `tanggal_punishment` date NOT NULL,
+  `id_karyawan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `reward`
+--
+
+CREATE TABLE `reward` (
+  `id_reward` int(11) NOT NULL,
+  `reward` text NOT NULL,
+  `keterangan` text NOT NULL,
+  `tanggal_reward` date NOT NULL,
+  `id_karyawan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama_lengkap` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama_lengkap`) VALUES
+(1, 'admin', '$2y$10$YyhiL0fHVqiT9MuTPSkFhO5Napx1HFY3qwe6TIzdSezFyJiR9N4bu', 'Administrator');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indeks untuk tabel `punishment`
+--
+ALTER TABLE `punishment`
+  ADD PRIMARY KEY (`id_punishment`),
+  ADD KEY `id_karyawan` (`id_karyawan`) USING BTREE;
+
+--
+-- Indeks untuk tabel `reward`
+--
+ALTER TABLE `reward`
+  ADD PRIMARY KEY (`id_reward`),
+  ADD KEY `id_karyawan` (`id_karyawan`) USING BTREE;
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `karyawan`
+--
+ALTER TABLE `karyawan`
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `punishment`
+--
+ALTER TABLE `punishment`
+  MODIFY `id_punishment` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `reward`
+--
+ALTER TABLE `reward`
+  MODIFY `id_reward` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `punishment`
+--
+ALTER TABLE `punishment`
+  ADD CONSTRAINT `punishment_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `reward`
+--
+ALTER TABLE `reward`
+  ADD CONSTRAINT `reward_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
